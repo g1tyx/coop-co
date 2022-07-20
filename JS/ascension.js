2373993096d64d160a4dda425696f63777a42017
MrBacon470 <Mrbacon470@outlook.com>
2022-07-20 10:39:20 -0500

    h

--- JS/ascension.js
+++ JS/ascension.js
@@ -44,8 +44,7 @@ function ascend() {
             data.epicResearch[i] = D(0)
         }
     }
-    for(let i = 0; i < 3; i++)
-        generateContract(i)
+    data.generatedContracts = false
     data.stats.timeInPrestige = D(0)
     data.hasPrestiged = true
     data.research = new Array(28).fill(D(0))
